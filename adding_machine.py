ea1bd5324e805c18e9d6e500015b13a78eff4f02
Phantom-Theif <94059203+Phantom-Theif@users.noreply.github.com>
2022-04-05 07:54:33 -0400

    Delete adding_machine.py

--- adding_machine.py
+++
@@ -1,5 +0,0 @@
-print("This is an adding machine")
-first_num = int(input("What is the first number you want to add?"))
-second_num = int(input("What is the second number you want to add?"))
-sum = first_num + second_num
-print("The sum of your two numbers are", sum)
