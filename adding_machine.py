8834852ae1774b90afb04edc63af53ee0e0d6250
Phantom-Theif <94059203+Phantom-Theif@users.noreply.github.com>
2022-04-05 07:55:05 -0400

    Add files via upload

---
+++ adding_machine.py
@@ -0,0 +1,13 @@
+from msvcrt import kbhit
+
+
+while True:
+    try:
+        print("This is an adding machine")
+        first_num = int(input("What is the first number you want to add? "))
+        second_num = int(input("What is the second number you want to add? "))
+        sum = first_num + second_num
+        print("The sum of your two numbers are", sum)
+        break
+    except:
+        print("your input is not correct...")
